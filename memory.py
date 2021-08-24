21335b9ec96923d651d20f371706ff497de90580
vishwas1703 <68908562+vishwas1703@users.noreply.github.com>
2021-08-24 15:31:25 +0530

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,1 +1,13 @@
-print("hello python")
+"""
+Memory, puzzle game of number pairs
+This program is checked for pylint
+author - Vishwas
+date -  22 aug 2021
+time - 6:30 am
+"""
+
+def add(a_n,b_n):
+    return a_n+b_n
+a_num=int(input())
+b_num=int(input())
+print(add(a_num,b_num))
